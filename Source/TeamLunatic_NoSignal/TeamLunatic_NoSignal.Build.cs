f080a27b6926ef5c60edda31e7fec7441e54e5de
Joel970203 <rngkfk0203@naver.com>
2025-05-12 18:03:29 +0900

    [Initialize Project]

--- Source/TeamLunatic_NoSignal/TeamLunatic_NoSignal.Build.cs
+++ Source/TeamLunatic_NoSignal/TeamLunatic_NoSignal.Build.cs
@@ -6,8 +6,8 @@ public class TeamLunatic_NoSignal : ModuleRules
 {
 	public TeamLunatic_NoSignal(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-	}
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;   
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","UMG" });
+        PublicIncludePaths.AddRange(new string[] { "TeamLunatic_NoSignal" });
+    }
 }
