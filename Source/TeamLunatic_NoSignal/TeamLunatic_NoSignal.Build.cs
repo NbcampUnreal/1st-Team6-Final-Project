3d17abf5e7e8efe043cf6458a6ba39f5b16056f1
지승현 <101084573+SeungH99@users.noreply.github.com>
2025-05-14 19:44:51 +0900

    Revert "Jsh 013 ai공격로직구현"

--- Source/TeamLunatic_NoSignal/TeamLunatic_NoSignal.Build.cs
+++ Source/TeamLunatic_NoSignal/TeamLunatic_NoSignal.Build.cs
@@ -7,7 +7,7 @@ public class TeamLunatic_NoSignal : ModuleRules
 	public TeamLunatic_NoSignal(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;   
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","UMG", "AIModule", "GameplayTasks", "NavigationSystem"});
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput","UMG" });
         PublicIncludePaths.AddRange(new string[] { "TeamLunatic_NoSignal" });
     }
 }
--- Source/TeamLunatic_NoSignal/Zombie/BT/Task/NS_BTTask_Attack.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Zombie/BT/Task/NS_BTTask_Attack.h"
-
-UNS_BTTask_Attack::UNS_BTTask_Attack()
-{
-}
