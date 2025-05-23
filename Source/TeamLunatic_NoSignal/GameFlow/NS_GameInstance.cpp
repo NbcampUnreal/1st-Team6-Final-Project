04bc52b41b0eb4b253d8095cae9cc4f102c436db
Joel970203 <rngkfk0203@naver.com>
2025-05-23 11:58:55 +0900

    [Update] 대기실 추가 & 게임인스턴스 코드 추가

--- Source/TeamLunatic_NoSignal/GameFlow/NS_GameInstance.cpp
+++ Source/TeamLunatic_NoSignal/GameFlow/NS_GameInstance.cpp
@@ -2,10 +2,43 @@
 
 
 #include "GameFlow/NS_GameInstance.h"
+#include "Kismet/GameplayStatics.h"
 #include "Engine/Engine.h"
 
 void UNS_GameInstance::SetGameModeType(EGameModeType Type)
 {
 	GameModeType = Type;
 	UE_LOG(LogTemp, Log, TEXT("[GameInstance] GameModeType set to %s"), *UEnum::GetValueAsString(Type));
+}
+
+void UNS_GameInstance::CreateSession(FName SessionName, bool bIsLAN, int32 MaxPlayers)
+{
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (!Subsystem) return;
+
+	IOnlineSessionPtr SessionInterface = Subsystem->GetSessionInterface();
+	if (!SessionInterface.IsValid()) return;
+
+	SessionSettings = MakeShareable(new FOnlineSessionSettings());
+	SessionSettings->bIsLANMatch = bIsLAN;
+	SessionSettings->NumPublicConnections = MaxPlayers;
+	SessionSettings->bShouldAdvertise = true;
+	SessionSettings->bUsesPresence = true;
+	SessionSettings->bUseLobbiesIfAvailable = true;
+
+	SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UNS_GameInstance::OnCreateSessionComplete);
+	SessionInterface->CreateSession(0, SessionName, *SessionSettings);
+}
+
+void UNS_GameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+	if (bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Log, TEXT("Session created successfully: %s"), *SessionName.ToString());
+		//UGameplayStatics::OpenLevel(GetWorld(), "대기실"); 
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to create session: %s"), *SessionName.ToString());
+	}
 }
--- Source/TeamLunatic_NoSignal/GameFlow/NS_GameInstance.h
+++ Source/TeamLunatic_NoSignal/GameFlow/NS_GameInstance.h
@@ -3,6 +3,8 @@
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "EGameModeType.h"  
+#include "Interfaces/OnlineSessionInterface.h"
+#include "OnlineSessionSettings.h"
 #include "NS_GameInstance.generated.h"
 
 UCLASS()
@@ -14,7 +16,12 @@ public:
 	void SetGameModeType(EGameModeType Type);
 	EGameModeType GetGameModeType() const { return GameModeType; }
 
+	void CreateSession(FName SessionName, bool bIsLAN, int32 MaxPlayers);
 
 private:
 	EGameModeType GameModeType = EGameModeType::SinglePlayMode;
+
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+
+	TSharedPtr<FOnlineSessionSettings> SessionSettings;
 };
