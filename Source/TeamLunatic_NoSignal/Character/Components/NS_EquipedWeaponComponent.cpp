664980ee5a984b34735a0b0c7b1721557cc29669
JangJinWoo <aktsksqkq@gmail.com>
2025-05-26 12:53:29 +0900

    [Fix]재장전 TODO명확히함, 캐릭터 90도 회전시 팔이 왔다갔다하는 이슈 수정중

--- Source/TeamLunatic_NoSignal/Character/Components/NS_EquipedWeaponComponent.cpp
+++ Source/TeamLunatic_NoSignal/Character/Components/NS_EquipedWeaponComponent.cpp
@@ -82,6 +82,7 @@ void UNS_EquipedWeaponComponent::MulticastEquipWeapon_Implementation(TSubclassOf
         CurrentWeapon = nullptr;
     }
 
+	// 무기 클래스가 유효해야함.(비무장 상태는 제외)
     if (WeaponClass && OwnerCharacter)
     {
         FActorSpawnParameters Params;
@@ -156,15 +157,17 @@ void UNS_EquipedWeaponComponent::StopAttack()
 void UNS_EquipedWeaponComponent::Reload()
 {
 	// 원거리 무장 아님 : 재장전 무조건 false
-	//if (CurrentWeapon && CurrentWeapon->GetWeaponType() != EWeaponType::Ranged)
-	//{
-	//	IsReload = false;
-	//	return;
-	//}
+	if (CurrentWeapon && CurrentWeapon->GetWeaponType() != EWeaponType::Ranged)
+	{
+		IsReload = false;
+		return;
+	}
 
     //TODO 인벤토리 살펴보기
     //인벤토리내 무기에 맞는 탄창or탄약이 있는지 체크
-    //if(탄창으로 구현) : 현재 탄창을 인벤토리에 넣고(부족하면)
+    //if(탄창으로 구현) : 현재 탄창을 인벤토리에 넣고 (인벤 칸 모자름 : 현재탄창 버려야함), 다음 탄창을 꺼내서 장착
+    //if(탄창으로 구현) : 다음 탄창이 뭔지 어떻게 알아야 할까? : 들어온 탄창순으로 고유번호를 주고 QUEUE구조로 돌려야할까? 아니면 대기표를 뽑아줄까?
+    //else if(탄약으로 구현) : 걍 최대 탄약수만큼 빼면 쉬움[탄창보다 딸깍이네]
 
     IsReload = true;
 }
