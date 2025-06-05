a3562fbff697319e49cc1255310d9e71002786f3
ChanYoung <parkcy723@naver.com>
2025-06-05 20:40:19 +0900

    [Feature] 캐릭터 사운드 추가 및 발차기 로직 삭제

--- Source/TeamLunatic_NoSignal/Character/NS_PlayerCharacterBase.h
+++ Source/TeamLunatic_NoSignal/Character/NS_PlayerCharacterBase.h
@@ -127,9 +127,6 @@ public:
 	// 재장전 실행 변수
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = "Replicated Variables")
 	bool IsReload = false;
-	// 발차기 확인 변수
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = "Replicated Variables")
-	bool IsKick = false;
 	// 아이템을 줍고있는지 확인 변수
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Replicated, Category = "Replicated Variables")
 	bool IsPickUp = false;
@@ -176,7 +173,7 @@ public:
 	UInputAction* ToggleMenuAction;
 	
 	
-	// 이동 입력 잠금 제어 함수 
+	// 캐릭터 EnhancedInput을 없앴다가 다시 부착하는는 함수 IMC를 지워웠다가 다시 장착하게해서 AnimNotify로 발차기 공격동안 IMC없앰
 	UFUNCTION(BlueprintCallable, Server, Reliable, Category="Input")
 	void SetMovementLockState_Server(bool bLock);
 	UFUNCTION(NetMulticast, Reliable)
@@ -200,11 +197,6 @@ public:
 	void StartSprint_Server(const FInputActionValue& Value);
 	UFUNCTION(Server, Reliable)
 	void StopSprint_Server(const FInputActionValue& Value);
-	
-	// 발차기
-	UFUNCTION(Server, Reliable)
-	void KickAction_Server(const FInputActionValue& Value);
-
 
 	// 아이템 줍기
 	UFUNCTION(Server, Reliable)
