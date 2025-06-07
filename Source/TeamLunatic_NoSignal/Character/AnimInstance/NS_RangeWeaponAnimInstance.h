a78feaed22ed29ef19036b14f0804365f8c59b98
ChanYoung <parkcy723@naver.com>
2025-06-07 14:34:32 +0900

    [Feature] 캐릭터 이펙트 작업 + 총 사격 애니메이션 추가 중간 세이브

---
+++ Source/TeamLunatic_NoSignal/Character/AnimInstance/NS_RangeWeaponAnimInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "NS_RangeWeaponAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMLUNATIC_NOSIGNAL_API UNS_RangeWeaponAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
