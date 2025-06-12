5d011442f4a8622eea774f72669ac10eab72df09
seung <seung991216@naver.com>
2025-06-12 20:49:21 +0900

    [Update] Runner좀비 사운드 및 Notify

--- Source/TeamLunatic_NoSignal/Zombie/Animations/Notify/NS_AnimNotify_CallTracker.cpp
+++ Source/TeamLunatic_NoSignal/Zombie/Animations/Notify/NS_AnimNotify_CallTracker.cpp
@@ -6,17 +6,45 @@
 #include "Kismet/GameplayStatics.h"
 #include "Zombie/NS_ZombieBase.h"
 #include "Zombie/AIController/NS_AIController.h"
+#include "Zombie/AIController/NS_ChaserController.h"
 
+
+UNS_AnimNotify_CallTracker::UNS_AnimNotify_CallTracker() : ScreamSound(nullptr), MaxHeardRange(3000.f), Loudness(1.f)
+{
+
+}
 
 void UNS_AnimNotify_CallTracker::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
                                         const FAnimNotifyEventReference& EventReference)
 {
+	if (TrackedActorBP)
+	{
+		UGameplayStatics::GetAllActorsOfClass(MeshComp->GetWorld(),TrackedActorBP, TrackerActors);
+	}
+	
 	ANS_ZombieBase* Zombie = Cast<ANS_ZombieBase>(MeshComp->GetOwner());
 	if (!Zombie) return;
 	ANS_AIController* Controller = Cast<ANS_AIController>(Zombie->GetController());
 	if (!Controller) return;
 	UBlackboardComponent* BlackboardComponent = Controller->GetBlackboardComponent();
 	if (!BlackboardComponent) return;
+
+	UObject* Key =  BlackboardComponent->GetValueAsObject("TargetActor");
+	if (!Key) return;
+	ANS_PlayerCharacterBase* Player = Cast<ANS_PlayerCharacterBase>(Key);
+	if (!Player) return;
+	
+	
+	for (AActor* Chacer : TrackerActors)
+	{
+		APawn* pawn = Cast<APawn>(Chacer);
+		if (!pawn) continue;
+		ANS_ChaserController* ChacerController = Cast<ANS_ChaserController>(pawn->GetController());
+		
+		if (!ChacerController) return;
+		
+		ChacerController->SetChaseTarget(Player, 60.f);
+	}
 
 	if (ScreamSound)
 	{
@@ -26,4 +54,5 @@ void UNS_AnimNotify_CallTracker::Notify(USkeletalMeshComponent* MeshComp, UAnimS
 		}
 		Zombie->Server_PlaySound(ScreamSound);
 	}
+	UAISense_Hearing::ReportNoiseEvent(MeshComp->GetWorld(), Zombie->GetActorLocation(),Loudness,Zombie, MaxHeardRange);
 }
--- Source/TeamLunatic_NoSignal/Zombie/Animations/Notify/NS_AnimNotify_CallTracker.h
+++ Source/TeamLunatic_NoSignal/Zombie/Animations/Notify/NS_AnimNotify_CallTracker.h
@@ -6,14 +6,26 @@
 #include "Animation/AnimNotifies/AnimNotify.h"
 #include "NS_AnimNotify_CallTracker.generated.h"
 
+class ANS_ChaserController;
+
 UCLASS()
 class TEAMLUNATIC_NOSIGNAL_API UNS_AnimNotify_CallTracker : public UAnimNotify
 {
 	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere,	BlueprintReadOnly, Category = "Actor")
+	TSubclassOf<AActor> TrackedActorBP;
+
 public:
+	UNS_AnimNotify_CallTracker();
+	float MaxHeardRange;
+	float Loudness;
+	
 	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	
 	UPROPERTY()
 	TArray<AActor*> TrackerActors;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Sound")
 	USoundCue* ScreamSound;
 };
