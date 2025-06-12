5c9307571e28df62dc42824cdc59335f198fea15
Joel970203 <rngkfk0203@naver.com>
2025-06-12 19:15:28 +0900

    [Update] Single Mode Random Spawn

--- Source/TeamLunatic_NoSignal/GameFlow/NS_SinglePlayMode.cpp
+++ Source/TeamLunatic_NoSignal/GameFlow/NS_SinglePlayMode.cpp
@@ -3,7 +3,6 @@
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/PlayerStart.h"
 #include "Engine/World.h"
-#include "EngineUtils.h"
 #include "Character/NS_PlayerCharacterBase.h"
 
 ANS_SinglePlayMode::ANS_SinglePlayMode()
@@ -13,11 +12,40 @@ ANS_SinglePlayMode::ANS_SinglePlayMode()
 void ANS_SinglePlayMode::BeginPlay()
 {
 	Super::BeginPlay();
+
+	UWorld* World = GetWorld();
+	if (!World || AvailablePawnClasses.Num() < 4) return;
+
+	const int32 RandIndex = FMath::RandRange(0, 3);
+	TSubclassOf<APawn> ChosenPawnClass = AvailablePawnClasses[RandIndex];
+
+	APlayerController* PC = UGameplayStatics::GetPlayerController(World, 0);
+	if (!PC || PC->GetPawn()) return;
+
+	AActor* Start = UGameplayStatics::GetActorOfClass(World, APlayerStart::StaticClass());
+	if (!Start) return;
+
+	FActorSpawnParameters Params;
+	Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	APawn* NewPawn = World->SpawnActor<APawn>(
+		ChosenPawnClass,
+		Start->GetActorLocation(),
+		Start->GetActorRotation(),
+		Params
+	);
+
+	if (NewPawn)
+	{
+		PC->Possess(NewPawn);
+		UE_LOG(LogTemp, Log, TEXT("싱글플레이: 랜덤 인덱스 %d 캐릭터 %s 스폰됨"), RandIndex, *NewPawn->GetName());
+	}
 }
 
 void ANS_SinglePlayMode::HandleGameOver(bool bPlayerSurvived, EEscapeRoute EscapeRoute)
 {
 	if (bIsGameOver) return;
+
 	bIsGameOver = true;
 	CurrentEscapeRoute = EscapeRoute;
 
@@ -30,7 +58,6 @@ void ANS_SinglePlayMode::HandleGameOver(bool bPlayerSurvived, EEscapeRoute Escap
 	{
 		UE_LOG(LogTemp, Warning, TEXT("GAME OVER! Player died."));
 	}
-
 }
 
 FVector ANS_SinglePlayMode::GetPlayerLocation_Implementation() const
--- Source/TeamLunatic_NoSignal/GameFlow/NS_SinglePlayMode.h
+++ Source/TeamLunatic_NoSignal/GameFlow/NS_SinglePlayMode.h
@@ -10,7 +10,7 @@ class AActor;
 class APawn;
 class ANS_PlayerCharacterBase;
 
-/** 탈출 루트 구분 */
+// 탈출 루트 구분 
 UENUM(BlueprintType)
 enum class EEscapeRoute : uint8
 {
@@ -19,7 +19,7 @@ enum class EEscapeRoute : uint8
 	Radio
 };
 
-/** 탈출 시도 정보 */
+// 탈출 시도 정보 
 USTRUCT(BlueprintType)
 struct FEscapeAttemptInfo
 {
@@ -29,7 +29,7 @@ struct FEscapeAttemptInfo
 	ANS_PlayerCharacterBase* Player = nullptr;
 
 	UPROPERTY(BlueprintReadWrite)
-	FName EscapeTargetTag = NAME_None; // "Car" or "Radio"
+	FName EscapeTargetTag = NAME_None;
 };
 
 UCLASS()
@@ -42,22 +42,15 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Location")
 	virtual FVector GetPlayerLocation_Implementation() const override;
+
 protected:
 	virtual void BeginPlay() override;
-
-	// 게임 종료 처리 
 	void HandleGameOver(bool bPlayerSurvived, EEscapeRoute EscapeRoute);
 
 private:
-	// 게임 상태 
+	UPROPERTY(EditAnywhere, Category = "Character")
+	TArray<TSubclassOf<APawn>> AvailablePawnClasses;
+
 	bool bIsGameOver = false;
 	EEscapeRoute CurrentEscapeRoute = EEscapeRoute::None;
-
-	// 참조 캐시 
-	UPROPERTY()
-	AActor* TrackerZombieInstance;
-
-	UPROPERTY()
-	AActor* CachedPlayerStart;
-
 };
