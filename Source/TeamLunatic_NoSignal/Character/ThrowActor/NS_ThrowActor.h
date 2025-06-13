c961dcac0ed106ebb79a9094e7e3a162db0380f6
ChanYoung <parkcy723@naver.com>
2025-06-13 11:20:41 +0900

    [Fix] 칼 애니메이션 수정 및 병 깨지는 사운드 1번만 실행되도록 버그 조치

--- Source/TeamLunatic_NoSignal/Character/ThrowActor/NS_ThrowActor.h
+++ Source/TeamLunatic_NoSignal/Character/ThrowActor/NS_ThrowActor.h
@@ -29,9 +29,13 @@ public:
 	// 땅이나 어디든 부딪쳤을때 부서질 지오메트리
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Throw")
 	TSubclassOf<AGeometryCollectionActor> FractureActorClass;
-	
-	// 병이 날아가는 방향 지정
-	void LaunchInDirection(const FVector& Direction);
+
+	// Hit오버렙 이벤트에서 출력될 사운드 변수
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	USoundBase* ImpactSound;
+
+	// 사운드 재생 여부 변수
+	bool bHasPlayedImpactSound = false;
 
 protected:
 	
@@ -41,5 +45,7 @@ public:
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor,
 			   UPrimitiveComponent* OtherComp, FVector NormalImpulse,
 			   const FHitResult& Hit);
-	
+
+	// 병이 날아가는 방향 지정
+	void LaunchInDirection(const FVector& Direction);
 };
