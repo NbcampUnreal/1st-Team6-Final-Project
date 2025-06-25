76ea9423b366b0bb1ad0cb286ccb5c171f924515
ChanYoung <parkcy723@naver.com>
2025-06-25 11:01:26 +0900

    [Fix] 게임 로고 변경

--- Source/TeamLunatic_NoSignal/UI/NS_LogoScreenWidget.h
+++ Source/TeamLunatic_NoSignal/UI/NS_LogoScreenWidget.h
@@ -15,13 +15,29 @@ class TEAMLUNATIC_NOSIGNAL_API UNS_LogoScreenWidget : public UUserWidget, public
 {
 	GENERATED_BODY()
 public:
-	//UPROPERTY(meta = (BindWidgetAnim), Transient)
+	// 첫 번째 로고 애니메이션
 	UPROPERTY(Transient, meta = (BindWidgetAnim))
 	UWidgetAnimation* ShowLogo;
+	
+	// 두 번째 로고 애니메이션
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+	UWidgetAnimation* ShowSecondLogo;
+	
+	// 메뉴로 전환하는 델리게이트
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnLogosFinished);
+	UPROPERTY(BlueprintAssignable, Category = "Logo")
+	FOnLogosFinished OnLogosFinished;
+	
 protected:
 	virtual void NativeConstruct() override;
 
 private:
 	UFUNCTION()
 	void OnShowLogoFinished();
+	
+	UFUNCTION()
+	void OnShowSecondLogoFinished();
+	
+	// 두 번째 로고 애니메이션 시작
+	void StartSecondLogoAnimation();
 };
