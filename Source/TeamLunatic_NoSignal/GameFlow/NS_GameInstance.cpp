d08a1561d8c6d3c73227b555a9b82fe2abe01ec7
Joel970203 <rngkfk0203@naver.com>
2025-06-25 17:44:29 +0900

    [Update] 가시성 증가
    WBP 글씨 가시성 증가 작업

--- Source/TeamLunatic_NoSignal/GameFlow/NS_GameInstance.cpp
+++ Source/TeamLunatic_NoSignal/GameFlow/NS_GameInstance.cpp
@@ -76,7 +76,7 @@ void UNS_GameInstance::CreateDedicatedSessionViaHTTP(FName SessionName, int32 Ma
 		*SessionName.ToString(), MaxPlayers);
 
 	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-	Request->SetURL(TEXT("http://121.163.249.108:5000/create_session"));
+	Request->SetURL(TEXT("http://13.124.224.194.108:5000/create_session"));
 	Request->SetVerb(TEXT("POST"));
 	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
 
@@ -141,7 +141,7 @@ void UNS_GameInstance::SendHeartbeat()
 		return;
 
 	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-	Request->SetURL(TEXT("http://121.163.249.108:5000/heartbeat"));
+	Request->SetURL(TEXT("http://13.124.224.194:5000/heartbeat"));
 	Request->SetVerb(TEXT("POST"));
 	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
 
@@ -174,7 +174,7 @@ void UNS_GameInstance::RequestUpdateSessionStatus(int32 Port, FString Status)
 	UE_LOG(LogTemp, Log, TEXT("[RequestUpdateSessionStatus] Sending HTTP POST to update session status. Port: %d, Status: %s"), Port, *Status);
 
 	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-	Request->SetURL(TEXT("http://121.163.249.108:5000/update_session_status")); 
+	Request->SetURL(TEXT("http://13.124.224.194:5000/update_session_status")); 
 	Request->SetVerb(TEXT("POST"));
 	Request->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
 
@@ -208,7 +208,7 @@ void UNS_GameInstance::OnUpdateSessionStatusResponse(FHttpRequestPtr Request, FH
 void UNS_GameInstance::RequestSessionListFromServer()
 {
 	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-	Request->SetURL(TEXT("http://121.163.249.108:5000/session_list"));
+	Request->SetURL(TEXT("http://13.124.224.194:5000/session_list"));
 	Request->SetVerb(TEXT("GET"));
 	Request->OnProcessRequestComplete().BindUObject(this, &UNS_GameInstance::OnReceiveSessionList);
 	Request->ProcessRequest();
