253971a60e40d8ff8b87416eefc977e917b52779
Cabbagggggggge <gpwjd7332@naver.com>
2025-06-27 03:25:43 +0900

    [Feature] 레벨 조정, BP 일부 수정, 코드 일부 수정
    공장 - 스폰 매니저 삭제, 주유소에 스폰 위치 설치, Pickup - ccd, 시뮬 키기, 9mm 머티리얼 수정, 스폰매니저 자동으로 삭제되게 하기.

--- Source/TeamLunatic_NoSignal/World/Pickup.cpp
+++ Source/TeamLunatic_NoSignal/World/Pickup.cpp
@@ -22,7 +22,7 @@ APickup::APickup()
 	SetReplicatingMovement(true);
 
 	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickupMesh");
-	PickupMesh->SetSimulatePhysics(false);
+	PickupMesh->SetSimulatePhysics(true);
 	SetRootComponent(PickupMesh);
 }
 
