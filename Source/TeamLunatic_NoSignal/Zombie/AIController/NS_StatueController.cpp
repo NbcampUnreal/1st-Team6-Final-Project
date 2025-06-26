380fa025211b8de246b59f86f437b108f549d5a2
Joel970203 <rngkfk0203@naver.com>
2025-06-26 12:14:53 +0900

    [Update] Fix

--- Source/TeamLunatic_NoSignal/Zombie/AIController/NS_StatueController.cpp
+++ Source/TeamLunatic_NoSignal/Zombie/AIController/NS_StatueController.cpp
@@ -1,20 +1,16 @@
-// NS_StatueController.cpp
 
 #include "NS_StatueController.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "BehaviorTree/BehaviorTreeComponent.h" // BrainComponent를 위해 추가
+#include "BehaviorTree/BehaviorTreeComponent.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
 
 ANS_StatueController::ANS_StatueController()
 {
-	// 1. 컴포넌트를 생성하여 '상속받은' 변수에 할당합니다.
-	//    헤더에서 선언을 지웠지만, 부모 클래스(AAIController)에 변수가 존재하므로 사용 가능합니다.
 	Blackboard = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackboardComponent"));
 	BrainComponent = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorTreeComponent"));
 
-	// 2. AIPerceptionComponent 생성 및 설정 (이 부분은 동일)
 	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
 	UAISenseConfig_Sight* SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("SightConfig"));
 	SightConfig->SightRadius = 3000.f;
@@ -22,8 +18,8 @@ ANS_StatueController::ANS_StatueController()
 	SightConfig->PeripheralVisionAngleDegrees = 90.f;
 	SightConfig->SetMaxAge(5.0f);
 	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
-	SightConfig->DetectionByAffiliation.bDetectNeutrals = false;
-	SightConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
 	AIPerceptionComponent->ConfigureSense(*SightConfig);
 	AIPerceptionComponent->SetDominantSense(SightConfig->GetSenseImplementation());
 }
@@ -32,13 +28,9 @@ void ANS_StatueController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
 
-	// 이 함수는 블랙보드가 유효한지 먼저 확인하는 것이 더 안전합니다.
 	if (Blackboard && BehaviorTreeAsset)
 	{
 		Blackboard->InitializeBlackboard(*BehaviorTreeAsset->BlackboardAsset);
-
-		// 비헤이비어 트리 실행은 BrainComponent를 통해 이루어집니다.
-		// RunBehaviorTree 함수가 내부적으로 BrainComponent를 사용합니다.
 		RunBehaviorTree(BehaviorTreeAsset);
 
 		AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &ANS_StatueController::OnTargetPerceptionUpdated);
--- Source/TeamLunatic_NoSignal/Zombie/AIController/NS_StatueController.h
+++ Source/TeamLunatic_NoSignal/Zombie/AIController/NS_StatueController.h
@@ -16,15 +16,11 @@ public:
 	ANS_StatueController();
 
 protected:
-	// 비헤이비어 트리 에셋만 멤버 변수로 가집니다.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AI")
 	TObjectPtr<class UBehaviorTree> BehaviorTreeAsset;
 
-	// AI 퍼셉션 컴포넌트
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
 	TObjectPtr<class UAIPerceptionComponent> AIPerceptionComponent;
-
-	// Blackboard와 BrainComponent 선언을 여기서 삭제했습니다.
 
 protected:
 	virtual void OnPossess(APawn* InPawn) override;
